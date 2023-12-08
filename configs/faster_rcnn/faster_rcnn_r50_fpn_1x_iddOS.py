49fff6ccec704f086ae85bb7e13ff6fa6559203a
1molna <74125405+1molna@users.noreply.github.com>
2023-12-08 13:58:53 +0800

    Add files via upload

---
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_iddOS.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn_iddOS.py',
+    '../_base_/datasets/iddOS.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
---
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_vocOS_f.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn_vocOS_f.py',
+    '../_base_/datasets/vocOS.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
